6542d700da8b3b27fac1b3ac2def67fe8be01ecf
Jens Albonius <jens.albonius@gmail.com>
2019-11-28 14:31:05 +0100

    Added basic project files
    - IMPORTANT! IntelliJ-users: Make sure you DELETE the by-project referenced Java version if you're using OpenJDK. Found in settings -> build, execution, deployment -> java compiler

---
+++ src/main/java/se/iotsundsvall/ingressmip/IngressMip.java
@@ -0,0 +1,12 @@
+package se.iotsundsvall.ingressmip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressMip {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngressMip.class, args);
+    }
+}
