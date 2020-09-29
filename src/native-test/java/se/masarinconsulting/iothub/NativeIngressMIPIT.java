b490101d0cac08aeade39b86eb33baee5664bf55
Isak Styf <isak.styf@masarinconsulting.se>
2020-09-29 22:38:53 +0200

    Remove .java files

--- src/native-test/java/se/masarinconsulting/iothub/NativeIngressMIPIT.java
+++
@@ -1,9 +0,0 @@
-package se.masarinconsulting.iothub;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeIngressMIPIT extends IngressMIPTest {
-
-    // Execute the same tests but in native mode.
-}
